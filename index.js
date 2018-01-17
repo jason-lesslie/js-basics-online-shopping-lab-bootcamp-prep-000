ebf9ccb07e4d5fe68f4e10b92e45981e27777659
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-17 01:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ removeFromCart("apples");
 
 function removeFromCart(item) {
   //Object.prototype.hasOwnProperty()
-  if ("true" == cart.hasOwnProperty(item)){
+  if ("true" == cart.entries().hasOwnProperty(item)){
     delete cart[item];
   } else {
     console.log("That item is not in your cart.");
