4476581a961f8bad533b362c8fc739552a81ff4e
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-17 00:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ function viewCart() {
       cartString += Object.keys(cart[i]).toString() + " at $" + Object.values(cart[i]).toString();
       if (cart.length == 2 & i == 0){
         cartString += " and "
-      } else if (cart.length > 2 & i == cart.length-2) {
-        cartString += ", and "
       } else if (cart.length > 2 & i < cart.length-2) {
         cartString += ", "
+      } else if (cart.length > 2 & i == cart.length-2) {
+        cartString += ", and "
       } else if (i == cart.length-1) {
         cartString += "."
       }
