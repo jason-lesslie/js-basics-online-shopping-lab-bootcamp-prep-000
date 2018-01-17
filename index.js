d1b9c2c55ac96275de5307d4b0a370dc3011c271
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-17 01:03:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function viewCart() {
 function total() {
   var total;
   for(let i=0; i < cart.length; i++){
-    total += Object.values(cart[i]);
+    total += Object.values(cart[i]).parseInt();
   }
   return total
 }
